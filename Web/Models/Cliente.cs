ae10376bcdeb796ab0114612fb58c9560a7fc932
Fabiana Chaves <fchaves203@gmail.com>
2024-09-29 12:23:33 -0300

    implementando telas e conexão ao banco

---
+++ Web/Models/Cliente.cs
@@ -0,0 +1,11 @@
+﻿namespace Web.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string telefone { get; set; }
+        public string email { get; set; }
+        public string cpf { get; set; }
+    }
+}
